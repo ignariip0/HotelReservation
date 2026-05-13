7866c48f32d1786555e9048750280d26ed8149e7
Cauê <cauepbrito0@email.com>
2026-05-13 19:50:48 -0300

    Very bad solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,66 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    private static DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(LocalDate checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(LocalDate checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public long duration() {
+        return ChronoUnit.DAYS.between(checkIn, checkOut);
+    }
+
+    public void updateDates(LocalDate checkIn, LocalDate checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + checkIn.format(df)
+                + ", check-out: "
+                + checkOut.format(df)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
